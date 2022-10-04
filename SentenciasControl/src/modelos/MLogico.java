d7260a822629b3258836723853937bfa84b5ae34
Laboratorio C205 <labc205@uam.net.ni>
2022-10-04 09:25:26 -0600

    Clase 04/10

---
+++ SentenciasControl/src/modelos/MLogico.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author labc205
+ */
+public class MLogico {
+       public boolean sonIguales(int num1, int num2){
+    
+        return num1==num2;
+       }
+       /**
+        * Verifica si dos numeros reales son iguales
+        * @param num1
+        * @param num2
+        * @return 
+        */
+       public boolean sonIguales (double num1, double num2){
+       
+           return num1==num2;
+       }
+       
+       
+             
+       
+}
